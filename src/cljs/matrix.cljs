58c1fb88402d107b9d029f68277d010ef95231c7
Mikael Hallendal <mhallendal@gmail.com>
2013-11-09 00:56:53 +0100

    Use hiccups.

--- src/cljs/matrix.cljs
+++ src/cljs/matrix.cljs
@@ -1,8 +1,10 @@
 (ns prima-matte.matrix
   (:use [clojure.string :only [join split]]
         [domina.xpath :only [xpath]])
+  (:require-macros [hiccups.core :as h])
   (:require [domina :as dom]
-            [domina.events :as event]))
+            [domina.events :as event]
+            [hiccups.runtime :as hiccupsrt]))
 
 (defn produce-row [func n others]
   (map #(func n %) others))
@@ -10,20 +12,19 @@
 (defn numbers-from-input-field [input-field]
   (split (dom/value input-field) #"\s+"))
 
-(defn make-html-tag [tag value]
-  (str "<" tag ">" value "</" tag ">"))
-
 (defn make-row [value]
-  (str "<tr class='number_row'><th>" value "</th>"
-       (map (partial make-html-tag "td") 
-            (produce-row - value (numbers-from-input-field (dom/by-id "top_values"))))
-       "</tr>"))
+  (h/html [:tr.number_row [:th value]
+       (map #(h/html [:td %])
+            (produce-row - value (numbers-from-input-field (dom/by-id "top_values"))))]))
 
 (defn update-input []
   (let [tbody (dom/by-id "results_table")]
     (dom/destroy! (dom/children tbody))
     (dom/append! tbody
-                 (str "<tr><td class='hidden'></td>" (join (map (partial make-html-tag "th") (numbers-from-input-field (dom/by-id "top_values")))) "</tr>"))
+                 (h/html 
+                   [:tr [:td.hidden] (join (map #(h/html [:th %])
+                                           (numbers-from-input-field (dom/by-id "top_values")))) 
+                          ]))
     (dom/append! tbody
                  (join (map make-row (numbers-from-input-field (dom/by-id "left_values")))))))
 
@@ -31,3 +32,7 @@
   (event/listen! (dom/by-id "top_values") :keyup update-input)
   (event/listen! (dom/by-id "left_values") :keyup update-input))
 
+;; Todo
+;; - Refactor update-input
+;; - Refactor make-roww
+;; - Support operator
